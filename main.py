5a45aa6f80526ccbfdf0c97749bc2d92c9fb66f6
iamtheratio <georgejmcmullan@gmail.com>
2025-07-04 22:20:20 -0400

    Added v2.0 in the program title.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ui import setup_ui
 
 def main():
     root = tk.Tk()
-    root.title("SMWC Downloader & Patcher")
+    root.title("SMWC Downloader & Patcher v2.0")
     root.geometry("900x850")
     setup_ui(root, run_pipeline)
     root.mainloop()
